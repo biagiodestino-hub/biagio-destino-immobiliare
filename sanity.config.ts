0c41d7c1bb77ab49a1184c1f1eb6df8681049fd3
Biagio Destino <biagiodestino@gmail.com>
2026-06-10 00:58:56 +0200

    Add files via upload

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,17 +1,20 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
 import { schemaTypes } from "./sanity/schemaTypes";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "missing-project-id";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
-const config = {
+const config = defineConfig({
   name: "biagio-destino-immobiliare",
   title: "Biagio Destino Immobiliare",
   projectId,
   dataset,
   basePath: "/studio",
+  plugins: [structureTool()],
   schema: {
     types: schemaTypes
   }
-};
+});
 
 export default config;
